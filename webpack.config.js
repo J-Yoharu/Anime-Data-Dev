649347fb836c7fdf2f35ad248cc9425cec5500e1
João Apostulo <japostulo@gmail.com>
2020-06-22 19:09:32 -0300

    Realizado configuração do webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./node_modules/regenerator-runtime/runtime.js','./src/index.js','./src/info.js'],
+  devtool:'source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
